41d39b3260be887450baac741f307c6d25324c41
hongbikim <>
2023-03-04 16:17:58 +0900

    API-5 Commit add evaluate metrics

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from sklearn.metrics import f1_score
+from sklearn.metrics import (accuracy_score, f1_score, precision_score,
+                             recall_score)
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 
@@ -33,9 +34,27 @@ def build_model(train_x, train_y):
 
 def evaluate(model, test_x, test_y):
     pred_y = model.predict(test_x)
-    score = f1_score(test_y, pred_y)
+    f1 = f1_score(test_y, pred_y)
+    accuracy = accuracy_score(test_y, pred_y)
+    precision = precision_score(test_y, pred_y)
+    recall = recall_score(test_y, pred_y)
+    score_dict = dict(f1=f1, accuracy=accuracy, precision=precision, recall=recall)
+    return score_dict
 
-    return score
+
+train_dir = "train.csv"
+test_dir = "test.csv"
+
+# Flow
+train, test = load_data(train_dir, test_dir)
+train_x, train_y, test_x, test_y = pre_processing(train, test)
+model = build_model(train_x, train_y)
+score_dict = evaluate(model, test_x, test_y)
+# print(score)
+
+pred_x = model.predict(test_x)
+
+print(score_dict)
 
 
 if __name__ == "__main__":
