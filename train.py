86aac43e0928695ee54766b1bedc55a32bb49f6c
Soohyun-Kim <soohyun.dev@gmail.com>
2023-03-04 16:41:59 +0900

    Api 3 (#20)

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ from sklearn.naive_bayes import GaussianNB
 def load_data(train_dir, test_dir):
     train = pd.read_csv(train_dir, index_col=["PassengerId"])
     test = pd.read_csv(test_dir, index_col=["PassengerId"])
+    train = train[: int(len(train) / 2)]
 
     return train, test
 
@@ -16,10 +17,10 @@ def pre_processing(train, test):
     train.loc[train["Sex"] == "female", "Sex"] = 1
     test.loc[test["Sex"] == "male", "Sex"] = 0
     test.loc[test["Sex"] == "female", "Sex"] = 1
-
-    feature_names = ["Pclass", "Sex", "Fare", "SibSp", "Parch"]
+    test["Fare"] = test["Fare"].fillna(0)
+    feature_names = ["Pclass", "Sex", "Fare", "SibSp"]
     train_x, train_y = train[feature_names], train["Survived"]
-    train_x, test_x, train_y, test_y = train_test_split(train_x, train_y, test_size=0.1)
+    test_x, test_y = test[feature_names], test["Sex"]
 
     return train_x, train_y, test_x, test_y
 
